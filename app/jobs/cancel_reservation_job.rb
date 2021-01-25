a6cd1d8c3c4f3f2da2d591834e67d001aaff5d72
Piotr Komorowski <piotr.komorowski@themasters.io>
2021-01-25 12:40:29 +0100

    update ticket quantity_left in cancel reservation job

--- app/jobs/cancel_reservation_job.rb
+++ app/jobs/cancel_reservation_job.rb
@@ -1,9 +1,20 @@
+# frozen_string_literal: true
+
 class CancelReservationJob < ApplicationJob
   queue_as :default
 
   def perform(reservation_id)
-    reservation = Reservation.find(reservation_id)
+    reservation = Reservation.includes(:tickets).find(reservation_id)
 
-    reservation.canceled! if reservation.reserved?
+    return unless reservation.reserved?
+
+    reservation.canceled!
+
+    ticket_types = reservation.tickets.group(:id).count
+    ticket_types.each do |tt|
+      ticket_type = TicketType.find(tt.first)
+      ticket_type.quantity_left += tt.second
+      ticket_type.save!
+    end
   end
 end
--- spec/jobs/cancel_reservation_job_spec.rb
+++ spec/jobs/cancel_reservation_job_spec.rb
@@ -1,7 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe CancelReservationJob, type: :job do
   let(:reservation) { create(:reservation) }
+  let(:ticket_type) { create(:ticket_type, event_id: reservation.event_id) }
+
+  before { create(:ticket, reservation_id: reservation.id, ticket_type_id: ticket_type.id) }
 
   it 'matches with enqueued job' do
     ActiveJob::Base.queue_adapter = :test
@@ -12,5 +17,9 @@ RSpec.describe CancelReservationJob, type: :job do
     described_class.perform_now(reservation.id)
 
     expect(Reservation.find(reservation.id).status).to eq 'canceled'
+  end
+
+  it 'adds quantity_left to ticket_type' do
+    expect { described_class.perform_now(reservation.id) }.to change { TicketType.find(ticket_type.id).quantity_left }.by(1)
   end
 end
